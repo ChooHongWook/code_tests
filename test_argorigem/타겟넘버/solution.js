a19dacfa79cece2fd3c9c6b5e3e11753a81726df
olt13mm <olt13mm@gmail.com>
2022-04-13 23:49:25 +0900

    Algorithm: 타겟넘버 완료

--- test_argorigem/타겟넘버/solution.js
+++ test_argorigem/타겟넘버/solution.js
@@ -1,30 +1,26 @@
 function solution(numbers, target) {
   let answer = 0;
-  let result;
 
   function recursion(sum, leftArr) {
     if (leftArr.length <= 0) {
-      return sum;
+      if (sum === target) {
+        answer++;
+      }
+      return;
     }
-    let bucket = [];
-    let head = leftArr[0];
-    let body = leftArr.slice(1);
-    console.log("head:", head);
-    console.log("body:", body);
     for (let i = 0; i < 2; i++) {
       if (i === 0) {
         // 다음 값이 더하거나
-        result = recursion(sum + head, body);
+        recursion(sum + leftArr[0], leftArr.slice(1));
       } else {
         // 다음값을 빼거나
-        result = recursion(sum - head, body);
+        recursion(sum - leftArr[0], leftArr.slice(1));
       }
     }
-
-    return result;
   }
-  console.log("25:", recursion(0, numbers));
+
+  recursion(0, numbers);
   return answer;
 }
 
-console.log(solution([1, 1, 1, 1, 1], 3));
+console.log("결과:", solution([1, 1, 1, 1, 1], 3));
