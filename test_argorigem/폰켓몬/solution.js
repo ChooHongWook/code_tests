155b8e6096c2bdbf5585a33cc47265194d3293ba
olt13mm <olt13mm@gmail.com>
2022-04-20 22:32:46 +0900

    Algorithm-Refactor: 폰켓몬

--- test_argorigem/폰켓몬/solution.js
+++ test_argorigem/폰켓몬/solution.js
@@ -8,3 +8,12 @@ function solution(nums) {
   answer = Math.min(uniqueArr.length, nums.length / 2);
   return answer;
 }
+
+// refactor
+function solution2(nums) {
+  // 1. 중복제거
+  const uniqueArr = [...new Set(nums)];
+  const max = nums.length / 2;
+  // 중복 제거된 길이와 원래 nums의 절반 길이중 작은것이 목표한 값
+  return uniqueArr.length > max ? max : uniqueArr.length;
+}
