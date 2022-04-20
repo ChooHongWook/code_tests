687fe9ecee66085ff80f4e25615486555f3fbf92
olt13mm <olt13mm@gmail.com>
2022-04-20 22:27:21 +0900

    Algorithm: 폰켓몬(done)

--- test_argorigem/폰켓몬/solution.js
+++ test_argorigem/폰켓몬/solution.js
@@ -1,4 +1,10 @@
 function solution(nums) {
-  var answer = 0;
+  let answer = 0;
+  // 1. 중복제거
+  const dupArr = nums.slice();
+  const set = new Set(dupArr);
+  const uniqueArr = [...set];
+  // 중복 제거된 길이와 원래 nums의 절반 길이중 작은것이 목표한 값
+  answer = Math.min(uniqueArr.length, nums.length / 2);
   return answer;
 }
