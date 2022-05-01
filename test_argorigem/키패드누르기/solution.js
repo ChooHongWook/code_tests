e95b3cd38d526b4604418240dd43ac10f4eef4fa
olt13mm <olt13mm@gmail.com>
2022-05-02 01:22:42 +0900

    Algorithm: 키패드누르기(문제 + 풀이 중간)

--- test_argorigem/키패드누르기/solution.js
+++ test_argorigem/키패드누르기/solution.js
@@ -1,4 +1,16 @@
 function solution(numbers, hand) {
-  var answer = '';
+  let right;
+  let left;
+  let answer = '';
+
+  for (let i = 0; i < number.length; i++) {
+    numbers[i];
+    if (numbers[i] === 1 || numbers[i] === 4 || numbers[i] === 7) {
+      answer += 'L';
+    } else if (numbers[i] === 3 || numbers[i] === 6 || numbers[i] === 9) {
+      answer += 'R';
+    } else {
+    }
+  }
   return answer;
 }
