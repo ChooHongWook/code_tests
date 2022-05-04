6a75ef9e89f6c391c213c6f06110ea0d773b413d
olt13mm <olt13mm@gmail.com>
2022-05-05 02:16:11 +0900

    Algorithm: 땅따먹기 풀기 + 다른사람 푸는것 공부

--- test_argorigem/행열의덧셈/solution.js
+++ test_argorigem/행열의덧셈/solution.js
@@ -1,4 +1,32 @@
+// 1안
 function solution(arr1, arr2) {
-  var answer = [[]];
-  return answer;
+  // arr1을 깊은 복사
+  let deepDup = JSON.parse(JSON.stringify(arr1));
+  //console.log("dup:",dup)
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr1[i].length; j++) {
+      //console.log("arr1[i][j]:",arr1[i][j])
+      deepDup[i][j] += arr2[i][j];
+    }
+  }
+  return deepDup;
+}
+// 깊은 삭제로 인해 시간이 많이 걸리는 것 같음
+
+// 2안 깊은복사 삭제
+function solution(arr1, arr2) {
+  //console.log("dup:",dup)
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr1[i].length; j++) {
+      //console.log("arr1[i][j]:",arr1[i][j])
+      arr1[i][j] += arr2[i][j];
+    }
+  }
+  return arr1;
+}
+//
+
+// 3안 가장짧은 코드
+function solution(arr1, arr2) {
+  return arr1.map((a, i) => a.map((b, j) => b + arr2[i][j]));
 }
--- test_argoritem2/땅따먹기/solution.js
+++ test_argoritem2/땅따먹기/solution.js
@@ -15,8 +15,95 @@ function solution(land) {
     }
   }
 
-  // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-  console.log('Hello Javascript');
-
   return answer;
+}
+//----------------------------------------------------------------
+// 아 전부다 꺼내서 비교를 해서 너무 오래걸림
+function solution(land) {
+  let result = [];
+  function repeat(arrs, num, idx) {
+    // 탈줄조건
+    if (arrs.length === 0) {
+      result.push(num);
+      console.log('pushresult:', num);
+      console.log();
+      console.log();
+      return;
+    }
+
+    let arr = arrs.shift();
+
+    for (let i = 0; i < arr.length; i++) {
+      // 전에 사용했었언 idx이면 넘어감
+      if (idx === i) {
+        continue;
+      }
+      console.log('arrs:', arrs);
+      console.log('arr:', arr);
+      console.log('i:', i);
+      console.log('num+arr[i]:', num + arr[i]);
+
+      repeat(arrs.slice(), num + arr[i], i);
+      console.log();
+    }
+  }
+
+  repeat(land, 0, 4);
+  console.log('result:', result);
+  return Math.max(...result);
+}
+//----------------------------------------------------------------
+
+// function solution(land) {
+//   for (let rowIndex = 1; rowIndex < land.length; rowIndex++) {
+//     for (let colIndex = 0; colIndex < land[0].length; colIndex++) {
+//       land[rowIndex][colIndex] += Math.max(
+//         ...land[rowIndex - 1].slice(0, colIndex),
+//         ...land[rowIndex - 1].slice(colIndex + 1)
+//       );
+//     }
+//   }
+//   return Math.max(...land[land.length - 1]);
+// }
+
+function solution(land) {
+  //열 인덱스만큼 반복
+  for (let rowIndex = 1; rowIndex < land.length; rowIndex++) {
+    // 행 인덱스 만큼 반복
+    for (let colIndex = 0; colIndex < land[0].length; colIndex++) {
+      // land의 각각의 위치에 기존값에 이젠에 더할수 있는 숫자중 가장 최댓갑 더하기
+      console.log('rowIndex:', rowIndex);
+      console.log('colIndex:', colIndex);
+      console.log('더하는 최대 배열:', [
+        ...land[rowIndex - 1].slice(0, colIndex),
+        ...land[rowIndex - 1].slice(colIndex + 1),
+      ]);
+      land[rowIndex][colIndex] += Math.max(
+        ...land[rowIndex - 1].slice(0, colIndex),
+        ...land[rowIndex - 1].slice(colIndex + 1)
+      );
+      console.log('land[rowIndex][colIndex]:', land[rowIndex][colIndex]);
+    }
+  }
+  console.log('최댓값 전의 배열확인:', land[land.length - 1]);
+  return Math.max(...land[land.length - 1]);
+}
+//----------------------------------------------------------------
+
+function solution(land) {
+  var answer = 0;
+
+  return Math.max(
+    ...land.reduce(
+      (a, c) => {
+        return [
+          c[0] + Math.max(a[1], a[2], a[3]),
+          c[1] + Math.max(a[0], a[2], a[3]),
+          c[2] + Math.max(a[0], a[1], a[3]),
+          c[3] + Math.max(a[0], a[1], a[2]),
+        ];
+      },
+      [0, 0, 0, 0]
+    )
+  );
 }
