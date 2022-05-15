f7a4c5e30e7d550742314e595612b34e45ac8422
olt13mm <olt13mm@gmail.com>
2022-05-15 21:52:29 +0900

    Algorithm:튜플(풀이)

--- test_algorithm2/튜플/solution.js
+++ test_algorithm2/튜플/solution.js
@@ -1,4 +1,28 @@
 function solution(s) {
-  var answer = [];
+  // 길이에 따른 추가된 숫자가 해당 위치에 들어감
+  let a = s.slice(2, s.length - 2);
+  // console.log("a:",a)
+  a = a.split('},{');
+  // console.log("a:",a)
+  a = a.map((e) => {
+    return e.split(',');
+  });
+  // console.log("a:",a)
+  let obj = {};
+  a.forEach((e) => {
+    // console.log("e:",e)
+    e.forEach((el) => {
+      if (obj[el] === undefined) {
+        obj[el] = 0;
+      }
+      obj[el] += 1;
+    });
+  });
+  // console.log("obj:",obj)
+  let answer = [];
+  for (let key in obj) {
+    answer[a.length - obj[key]] = Number(key);
+  }
+
   return answer;
 }
