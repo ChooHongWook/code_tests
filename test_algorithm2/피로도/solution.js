daf6d922ff1f46827d635d61f0c52339b9a8d817
olt13mm <olt13mm@gmail.com>
2022-05-29 08:52:47 +0900

    Algorithm2: 피로도(시도)

--- test_algorithm2/피로도/solution.js
+++ test_algorithm2/피로도/solution.js
@@ -1,4 +1,14 @@
 function solution(k, dungeons) {
+  for (let i = 0; i < dungeons.length; i++) {
+    dungeons[i];
+  }
   var answer = -1;
   return answer;
 }
+
+let result = solution(80, [
+  [80, 20],
+  [50, 40],
+  [30, 10],
+]);
+console.log('result:', result);
