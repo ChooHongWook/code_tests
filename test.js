12d04afbaca245912a126e8bf5897159d2240dd0
olt13mm <olt13mm@gmail.com>
2022-05-30 07:48:58 +0900

    Study:공부

--- test.js
+++ test.js
@@ -1,10 +1,25 @@
-let arr = [];
+function mostFrequentCharacter(str) {
+  // TODO: 여기에 코드를 작성합니다.
 
-while (arr.length === 0) {
-  arr.push(1);
-  let a = 4;
-  var b = 5;
+  let newObj = {};
+  let result = '';
+  let bigNum = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === ' ') {
+      continue;
+    }
+
+    if (str[i] in newObj) {
+      newObj[str[i]] += 1;
+      if (bigNum < newObj[str[i]]) {
+        result = str[i];
+        bigNum = newObj[str[i]];
+      }
+    } else {
+      newObj[str[i]] = 1;
+    }
+  }
+
+  return result;
 }
-
-// console.log('a:', a);
-console.log('b:', b);
