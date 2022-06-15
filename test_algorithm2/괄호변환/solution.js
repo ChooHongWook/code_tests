26495be03dd57be1a01f84b772d6d3ecbbb4344e
olt13mm <olt13mm@gmail.com>
2022-06-15 10:01:14 +0900

    Algorithm: 괄호변환(풀이)

--- test_algorithm2/괄호변환/solution.js
+++ test_algorithm2/괄호변환/solution.js
@@ -1,3 +1,5 @@
+// 수정중
+
 function solution(p) {
   if (p === '') return '';
   if (isCorrected(p)) return p;
@@ -8,18 +10,24 @@ function solution(p) {
   let u = '',
     v = '';
   for (let i = 0; i < p.length; i++) {
-    console.log('slice', p.slice(0, i));
     let sliced = p.slice(0, i);
+    console.log('sliced:', sliced);
 
     if (isBalanced(sliced)) {
       u = sliced;
       v = p.slice(i);
+      console.log('u:', u);
+      console.log('v:', v);
+
       if (isCorrected(u)) {
         result += u;
+        result += solution(v);
       } else {
+        let aa = '(';
         let buff = u.slice(1, -1);
       }
     }
+    console.log();
   }
   var answer = '';
   return answer;
@@ -59,3 +67,29 @@ function isCorrected(str) {
 let result = solution('()))((()');
 
 console.log('result: ', result);
+
+//----------------------------------------------------------------
+// 풀이
+function solution(p) {
+  if (p.length === 0) return p;
+  let bracketCount = 0;
+  let isURight = true;
+  for (let i = 0; i < p.length; i++) {
+    bracketCount = p[i] === '(' ? bracketCount + 1 : bracketCount - 1;
+    if (bracketCount < 0) isURight = false;
+    if (bracketCount === 0) {
+      const [u, v] = [p.slice(0, i + 1), p.slice(i + 1)];
+      if (isURight) {
+        return u + solution(v);
+      } else {
+        let emptyString = '(' + solution(v) + ')';
+        const slicedReversedString = u
+          .slice(1, u.length - 1)
+          .split('')
+          .map((bracket) => (bracket === '(' ? ')' : '('))
+          .join('');
+        return emptyString + slicedReversedString;
+      }
+    }
+  }
+}
