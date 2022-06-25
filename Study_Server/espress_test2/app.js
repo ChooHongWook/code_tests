540fff52ab1cca9db986d53af72dada6e25c6be8
olt13mm <olt13mm@gmail.com>
2022-06-25 22:30:34 +0900

    Study: express study

--- Study_Server/espress_test2/app.js
+++ Study_Server/espress_test2/app.js
@@ -1,10 +1,16 @@
-const exports = require('express');
+const express = require('express');
 
 const app = express();
+
+const members = require('./members');
+
+console.log('members:', members);
 
 app.get('/main', (req, res) => {
   res.send('<h1>Main Page</h1>');
 });
 
 let port = 4000;
-app.listen(port);
+app.listen(port, () => {
+  console.log(`Server is running port:${port}`);
+});
---
+++ Study_Server/espress_test2/members.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Kim',
+    team: 'engineering',
+  },
+  {
+    id: 2,
+    name: 'Jon',
+    team: 'engineering',
+  },
+  {
+    id: 3,
+    name: 'Kim',
+    team: 'engineering',
+  },
+  {
+    id: 4,
+    name: 'Park',
+    team: 'engineering',
+  },
+];
