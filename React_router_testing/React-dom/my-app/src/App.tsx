1524d8701841b100e7e787ae5132f8c23d1af442
olt13mm <olt13mm@gmail.com>
2022-11-09 23:36:04 +0900

    2022/11/09 study

--- React_router_testing/React-dom/my-app/src/App.tsx
+++ React_router_testing/React-dom/my-app/src/App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 import {
   BrowserRouter as Router,
   Routes,
@@ -8,22 +8,25 @@ import {
   useNavigate,
   useLocation,
   useParams,
-} from "react-router-dom";
-import { usePrompt } from "./component/Blocker";
-import Main from "./page/Mypage";
-import Mypage from "./page/Mypage";
+} from 'react-router-dom';
+import { usePrompt } from './component/Blocker';
+import Main from './page/Mypage';
+import Mypage from './page/Mypage';
 // v5 -> v6
 //Switch -> Routes
 // useHistory -> useNavigate
 const App: React.FC = () => {
   // usePrompt("현재 페이지를 벗어나시겠습니까?", true);
-
+  console.log('연결됨?ㄴ');
   return (
     <div className="App">
-      <Routes>
-        <Route path="/main" element={<Main />} />
-        <Route path="/detail" element={<Mypage />} />
-      </Routes>
+      {/* ㅇㅕ기쯤?ㄴ */}
+      <Router>
+        <Routes>
+          <Route path="/main" element={<Main />} />
+          <Route path="/detail" element={<Mypage />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
